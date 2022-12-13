4722f53631d4ebcafa0d8162037a311aabf418b9
Marcello Venturini <marcellofventurini@gmail.com>
2022-12-12 22:15:21 -0300

    First commit - Project created

---
+++ src/main/java/com/marcelloventurini/springmongodb/SpringMongodbApplication.java
@@ -0,0 +1,11 @@
+package com.marcelloventurini.springmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongodbApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMongodbApplication.class, args);
+  }
+}
