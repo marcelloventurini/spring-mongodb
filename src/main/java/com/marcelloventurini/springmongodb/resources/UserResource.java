bc8c1d33052ae4b65a3961a5ff0e077f5fd6a764
Marcello Venturini <marcellofventurini@gmail.com>
2022-12-16 00:35:46 -0300

    User update

--- src/main/java/com/marcelloventurini/springmongodb/resources/UserResource.java
+++ src/main/java/com/marcelloventurini/springmongodb/resources/UserResource.java
@@ -43,4 +43,12 @@ public class UserResource {
     userService.delete(id);
     return ResponseEntity.noContent().build();
   }
+
+  @PutMapping(value = "/{id}")
+  public ResponseEntity<Void> update(@RequestBody UserDTO userDTO, @PathVariable String id) {
+    User user = userService.fromDTO(userDTO);
+    user.setId(id);
+    user = userService.update(user);
+    return ResponseEntity.noContent().build();
+  }
 }
--- src/main/java/com/marcelloventurini/springmongodb/services/UserService.java
+++ src/main/java/com/marcelloventurini/springmongodb/services/UserService.java
@@ -33,6 +33,17 @@ public class UserService {
     userRepository.deleteById(id);
   }
 
+  public User update(User user) {
+    User newUser = findById(user.getId());
+    updateData(newUser, user);
+    return userRepository.save(newUser);
+  }
+
+  public void updateData(User newUser, User user) {
+    newUser.setName(user.getName());
+    newUser.setEmail(user.getEmail());
+  }
+
   public User fromDTO(UserDTO userDTO) {
     return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
   }
