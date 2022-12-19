e16b3553f83a5c145fc9fea85169b4419c48c9c0
Marcello Venturini <marcellofventurini@gmail.com>
2022-12-19 14:42:30 -0300

    Reference for User's posts

--- src/main/java/com/marcelloventurini/springmongodb/configs/Instantiation.java
+++ src/main/java/com/marcelloventurini/springmongodb/configs/Instantiation.java
@@ -35,5 +35,8 @@ public class Instantiation implements CommandLineRunner {
     Post p2 = new Post(null, Instant.parse("2022-06-22T10:22:07Z"), "Good morning", "It's raining here in Sao Paulo.", new AuthorDTO(maria));
 
     postRepository.saveAll(Arrays.asList(p1, p2));
+
+    maria.getPosts().addAll(Arrays.asList(p1, p2));
+    userRepository.save(maria);
   }
 }
